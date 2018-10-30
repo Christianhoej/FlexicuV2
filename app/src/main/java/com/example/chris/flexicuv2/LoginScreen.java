969c3f665c67cafc46a2a3812725eb82278756f4
Gunn Hentze <s145494@student.dtu.dk>
2018-10-30 22:30:59 +0100

    Log ind + opret bruger skærm.

--- app/src/main/java/com/example/chris/flexicuv2/LoginScreen.java
+++ app/src/main/java/com/example/chris/flexicuv2/LoginScreen.java
@@ -3,17 +3,34 @@ package com.example.chris.flexicuv2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginScreen extends AppCompatActivity implements View.OnClickListener {
+    private Button logIn;
+    private Button newUser;
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
-        Button tilStart = (Button) findViewById(R.id.toStartBtn);
-        tilStart.setOnClickListener(this);
+
+        logIn = findViewById(R.id.logInBtn);
+        logIn.setOnClickListener(this);
+
+        newUser = findViewById(R.id.newUser);
+        newUser.setOnClickListener(this);
+
+        username = findViewById(R.id.emailInput);
+        username.setHint("Indtast email");
+
+        password = findViewById(R.id.passwordInput);
+        password.setHint("Indtast adgangskode");
     }
 
     public void openStartScreen(){
@@ -21,8 +38,50 @@ public class LoginScreen extends AppCompatActivity implements View.OnClickListen
         startActivity(intent);
     }
 
+    public void openNewUserScreen(){
+        Intent intent = new Intent(this, NewUserScreen.class);
+        startActivity(intent);
+    }
+
     @Override
     public void onClick(View v) {
-        openStartScreen();
+        boolean rightInput = true;
+        // If the login button is pressed
+        if(v.getId() == logIn.getId()){
+            // If the email edittext is not empty
+            if(!isEmpty(username)){
+                // If the email is not the format of an email
+                if(!Patterns.EMAIL_ADDRESS.matcher(username.getText().toString()).matches()){
+                    username.setError("Den indtastede email adresse eksisterer ikke");
+                    rightInput = false;
+                }
+                else{
+                    // if password is empty
+                    if(isEmpty(password)) {
+                        password.setError("Du skal indtaste et password");
+                        rightInput = false;
+                    }
+                }
+            }
+            else{
+                username.setError("Du skal indtaste en email adresse");
+                rightInput = false;
+            }
+
+            // if all conditions above is met, open startscreen
+            if(rightInput) {
+                openStartScreen();
+            }
+        }
+        // if the new user button is pressed
+        else if(v.getId() == newUser.getId()){
+            openNewUserScreen();
+        }
+    }
+
+
+
+    public boolean isEmpty(EditText text){
+        return TextUtils.isEmpty(text.getText().toString());
     }
 }
