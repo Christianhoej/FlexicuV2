14b2e94650a720509d95c2ce24be3533ff245c38
Christianhoej <s175125@student.dtu.dk>
2019-01-15 14:42:00 +0100

    rotation

--- app/src/main/java/com/example/chris/flexicuv2/loadingscreen/Loading_Screen.java
+++ app/src/main/java/com/example/chris/flexicuv2/loadingscreen/Loading_Screen.java
@@ -31,14 +31,20 @@ public class Loading_Screen extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_loading__screen, container, false);
 
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_loading__screen, container, false);
+
         tv = v.findViewById(R.id.loading_text);
         ivX = v.findViewById(R.id.x_logo);
-        rotateAnim =AnimationUtils.loadAnimation(getActivity(), R.anim.rotation);//TODO link fragmentet til en "kontekst"
+        /*rotateAnim =AnimationUtils.loadAnimation(getActivity(), R.anim.rotation);//TODO link fragmentet til en "kontekst"
         ivX.startAnimation(rotateAnim);
         Log.d("anim","Kommer her");
+        */
+
+        Animation animation = AnimationUtils.loadAnimation(getActivity(), R.anim.rotation);
+        ivX.startAnimation(animation);
+
         return v;
     }
 
