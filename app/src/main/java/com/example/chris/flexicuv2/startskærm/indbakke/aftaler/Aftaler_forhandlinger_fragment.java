f1d34a424d5181dcd2291bab11ccaf17974eb6e5
Christianhoej <s175125@student.dtu.dk>
2019-01-19 17:21:08 +0100

    Diverse design & fragmenter i framelayout

--- app/src/main/java/com/example/chris/flexicuv2/startskærm/indbakke/aftaler/Aftaler_forhandlinger_fragment.java
+++ app/src/main/java/com/example/chris/flexicuv2/startskærm/indbakke/aftaler/Aftaler_forhandlinger_fragment.java
@@ -3,11 +3,13 @@ package com.example.chris.flexicuv2.startskærm.indbakke.aftaler;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.chris.flexicuv2.R;
 import com.example.chris.flexicuv2.startskærm.indbakke.forhandling.Forhandling;
@@ -21,6 +23,10 @@ public class Aftaler_forhandlinger_fragment extends Fragment implements View.OnC
     private RecyclerView recyclerView;
     Forhandling_recyclerview_adapter adapter;
 
+    Button åben_forhandling;
+
+    Forhandling forhandlingFragment;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.aftaler_forhandlinger_fragment, container, false);
@@ -28,13 +34,18 @@ public class Aftaler_forhandlinger_fragment extends Fragment implements View.OnC
         recyclerView = (RecyclerView) v.findViewById(R.id.forhandlinger_recyclerview);
         recyclerView.setOnClickListener(this);
 
+        åben_forhandling = v.findViewById(R.id.vis_forhandling);
+        åben_forhandling.setOnClickListener(this);
+
+        forhandlingFragment = new Forhandling();
+
         fyldRecyclerView(v);
         return v;
     }
 
     @Override
     public void onClick(View v) {
-
+        setFragment(forhandlingFragment);
     }
 
     private void fyldRecyclerView(View v){
@@ -43,5 +54,13 @@ public class Aftaler_forhandlinger_fragment extends Fragment implements View.OnC
         adapter = new Forhandling_recyclerview_adapter(getContext());
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    public void setFragment(Fragment fragment) {
+        //startskærmFrameTilDiverse.removeAllViews();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.startskærm_frame_til_diverse, fragment);
+        fragmentTransaction.addToBackStack("fragment");
+        fragmentTransaction.commit();
     }
 }
