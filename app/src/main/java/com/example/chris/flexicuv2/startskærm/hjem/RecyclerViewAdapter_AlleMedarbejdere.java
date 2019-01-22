bc373fc018c77d71fbd725770d84227f59c2fe66
Christianhoej <s175125@student.dtu.dk>
2019-01-22 18:48:44 +0100

    Hjemskærm clickable

--- app/src/main/java/com/example/chris/flexicuv2/startskærm/hjem/RecyclerViewAdapter_AlleMedarbejdere.java
+++ app/src/main/java/com/example/chris/flexicuv2/startskærm/hjem/RecyclerViewAdapter_AlleMedarbejdere.java
@@ -1,23 +1,42 @@
 package com.example.chris.flexicuv2.startskærm.hjem;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.chris.flexicuv2.R;
+import com.example.chris.flexicuv2.medarbejdere.Rediger_medarbejder_fragment_1;
 import com.example.chris.flexicuv2.model.Medarbejder;
+import com.example.chris.flexicuv2.model.Singleton;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 public class RecyclerViewAdapter_AlleMedarbejdere extends RecyclerView.Adapter<RecyclerViewAdapter_AlleMedarbejdere.ViewHolder>{
 
     private ArrayList<Medarbejder> mMedarbejder;
     private Context mContext;
+    Singleton singleton;
+    private Rediger_medarbejder_fragment_1 rediger_medarbejder_fragment_1;
 
     public RecyclerViewAdapter_AlleMedarbejdere(Context mContext, ArrayList<Medarbejder> mMedarbejder) {
         this.mMedarbejder = mMedarbejder;
@@ -34,11 +53,22 @@ public class RecyclerViewAdapter_AlleMedarbejdere extends RecyclerView.Adapter<R
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerViewAdapter_AlleMedarbejdere.ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter_AlleMedarbejdere.ViewHolder viewHolder, final int i) {
         String loen = "Løn: " + Integer.toString(mMedarbejder.get(i).getLoen());
         viewHolder.name.setText(mMedarbejder.get(i).getNavn());
         //viewHolder.salary.setText(loen);
         viewHolder.workfield.setText(mMedarbejder.get(i).getArbejdsomraade());
+
+        viewHolder.alle_medarbejdere_listItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Log.d(TAG, "onClick: clicked on" + singleton.getMedarbejdere().get(i).getNavn());
+
+                onButtonShowPopupWindowClick(v, i);
+                //Toast.makeText(mContext, mMedarbejderNavn.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 
@@ -48,16 +78,98 @@ public class RecyclerViewAdapter_AlleMedarbejdere extends RecyclerView.Adapter<R
         return mMedarbejder.size();
     }
 
+
     public class ViewHolder extends RecyclerView.ViewHolder{
         private TextView name;
         //private TextView salary;
         private TextView workfield;
+        RelativeLayout alle_medarbejdere_listItem;
 
         public ViewHolder(View itemView){
             super(itemView);
             name = itemView.findViewById(R.id.medarbejderNavn);
             //salary = itemView.findViewById(R.id.salary);
             workfield = itemView.findViewById(R.id.workfield);
+            alle_medarbejdere_listItem = itemView.findViewById(R.id.alle_medarbejdere_start_listItem);
         }
     }
+
+    public void onButtonShowPopupWindowClick(View view, final int i) {
+        //tilPopUp = (View) findViewById(R.id.tilPopUp);
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View popupView = inflater.inflate(R.layout.hjem_popup_alle, null);
+
+        TextView navn = popupView.findViewById(R.id.medarbejder_navn);
+        navn.setText("Navn: " + singleton.getMedarbejdere().get(i).getNavn());
+
+        TextView arbejdsområde = popupView.findViewById(R.id.medarbejder_arbejdsområde);
+        arbejdsområde.setText("Arbejdsområde: " + singleton.getMedarbejdere().get(i).getArbejdsomraade());
+
+        TextView køn = popupView.findViewById(R.id.medarbejder_køn);
+        køn.setText("Køn: " + singleton.getMedarbejdere().get(i).getKøn());
+
+        TextView email = popupView.findViewById(R.id.medarbejder_email);
+        email.setText("Email: " + singleton.getMedarbejdere().get(i).getEmail());
+
+        TextView alder = popupView.findViewById(R.id.medarbejder_alder);
+        alder.setText("Alder: " + (Calendar.getInstance().get(Calendar.YEAR)-singleton.getMedarbejdere().get(i).getFødselsår()) + " år");
+
+        TextView adresse = popupView.findViewById(R.id.medarbejder_adresse);
+        adresse.setText("Adresse: " + singleton.getMedarbejdere().get(i).getVejnavn() + " " + singleton.getMedarbejdere().get(i).getHusnummer() + ", " + singleton.getMedarbejdere().get(i).getPostnr());
+
+
+        //get width
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        ((Activity)mContext).getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int width = displaymetrics.widthPixels;
+
+        // create the popup window
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, (width-20), height, focusable);
+        //popupWindow.setWidth(LinearLayout.LayoutParams.MATCH_PARENT);
+
+        //final PopupWindow popupWindowt = new PopupWindow();
+        popupWindow.setContentView(popupView);
+        popupWindow.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+        popupWindow.setElevation(20);
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+        //  popupWindow.setOutsideTouchable(true);
+            /*// dismiss the popup window when touched
+            popupView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    popupWindow.dismiss();
+                    return true;
+                }
+            });*/
+ /*       final Button rediger = popupView.findViewById(R.id.rediger);
+        rediger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rediger_medarbejder_fragment_1 = new Rediger_medarbejder_fragment_1();
+                openSkaerm(Medarbejdere_skaerm_akt.class);
+
+                singleton.midlertidigMedarbejder = singleton.getMedarbejdere().get(i);
+                popupWindow.dismiss();
+            }
+        });
+*/
+    }
+    public void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = ((AppCompatActivity)mContext).getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.medarbejdere_frame, fragment);
+        fragmentTransaction.commit();
+    }
+
+    public void openSkaerm(Class a){
+        Intent intent = new Intent(((AppCompatActivity)mContext),a);
+        mContext.startActivity(intent);
+        //setFragment(rediger_medarbejder_fragment_1);
+    }
+
+
 }
--- app/src/main/java/com/example/chris/flexicuv2/startskærm/hjem/RecyclerViewAdapter_Udlej.java
+++ app/src/main/java/com/example/chris/flexicuv2/startskærm/hjem/RecyclerViewAdapter_Udlej.java
@@ -1,23 +1,35 @@
 package com.example.chris.flexicuv2.startskærm.hjem;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.chris.flexicuv2.R;
 import com.example.chris.flexicuv2.model.Medarbejder;
+import com.example.chris.flexicuv2.model.Singleton;
 
 import java.util.ArrayList;
 import java.util.Collections;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 public class RecyclerViewAdapter_Udlej extends RecyclerView.Adapter<RecyclerViewAdapter_Udlej.ViewHolder>  {
 
     private ArrayList<Medarbejder> mMedarbejder;
     private Context mContext;
+    private Singleton singleton;
 
     public RecyclerViewAdapter_Udlej(Context mContext, ArrayList<Medarbejder> mMedarbejder) {
         this.mMedarbejder = mMedarbejder;
@@ -34,12 +46,23 @@ public class RecyclerViewAdapter_Udlej extends RecyclerView.Adapter<RecyclerView
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
 
+        singleton = Singleton.getInstance();
         String loen = "Løn: " + Integer.toString(mMedarbejder.get(i).getLoen());
         viewHolder.name.setText(mMedarbejder.get(i).getNavn());
         viewHolder.salary.setText(loen);
         viewHolder.workfield.setText(mMedarbejder.get(i).getArbejdsomraade());
+
+        viewHolder.hjemLejListItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.d(TAG, "onClick: clicked on" + singleton.getMedarbejdere().get(i).getNavn());
+
+                onButtonShowPopupWindowClick(v, i);
+                //Toast.makeText(mContext, mMedarbejderNavn.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
@@ -54,6 +77,7 @@ public class RecyclerViewAdapter_Udlej extends RecyclerView.Adapter<RecyclerView
         private TextView period;
         private TextView salary;
         private TextView workfield;
+        private RelativeLayout hjemLejListItem;
 
         public ViewHolder(View itemView){
             super(itemView);
@@ -61,6 +85,74 @@ public class RecyclerViewAdapter_Udlej extends RecyclerView.Adapter<RecyclerView
             period = itemView.findViewById(R.id.periode);
             salary = itemView.findViewById(R.id.salary);
             workfield = itemView.findViewById(R.id.workfield);
+            hjemLejListItem = itemView.findViewById(R.id.hjem_lej_listItem);
         }
+    }
+
+    /**
+     * https://stackoverflow.com/questions/5944987/how-to-create-a-popup-window-popupwindow-in-android
+     * @param view
+     */
+    public void onButtonShowPopupWindowClick(View view, final int i) {
+
+        //TODO Når lejede aftaler er implementeret skal det her opdateres;
+        //tilPopUp = (View) findViewById(R.id.tilPopUp);
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View popupView = inflater.inflate(R.layout.hjem_popup_aftaler, null);
+
+        TextView navn = popupView.findViewById(R.id.aftaler_navn);
+        navn.setText("Navn: " + singleton.getMedarbejdere().get(i).getNavn());
+
+        TextView arbejdsområde = popupView.findViewById(R.id.aftaler_arbejdsområder);
+        arbejdsområde.setText("Arbejdsområder: " + singleton.getMedarbejdere().get(i).getArbejdsomraade());
+
+        TextView periode = popupView.findViewById(R.id.aftaler_periode);
+        periode.setText("Periode: TILFØJ" );
+
+        TextView dage = popupView.findViewById(R.id.aftaler_arbejdsdage);
+        dage.setText("Dage: TILFØJ" );
+
+        TextView virksomhed = popupView.findViewById(R.id.aftaler_virksomhed);
+        virksomhed.setText("Virksomhed: TILFØJ");
+
+        TextView værktøj = popupView.findViewById(R.id.aftaler_værktøj);
+        værktøj.setText("Virksomhed: TILFØJ");
+
+        TextView timepris = popupView.findViewById(R.id.aftaler_timepris);
+        timepris.setText("Timepris: TILFØJ");
+
+        TextView total = popupView.findViewById(R.id.aftaler_totalpris);
+        total.setText("Totalpris: TILFØJ");
+
+
+        //get width
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        ((Activity)mContext).getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int width = displaymetrics.widthPixels;
+
+        // create the popup window
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, (width-20), height, focusable);
+        //popupWindow.setWidth(LinearLayout.LayoutParams.MATCH_PARENT);
+
+        //final PopupWindow popupWindowt = new PopupWindow();
+        popupWindow.setContentView(popupView);
+        popupWindow.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+        popupWindow.setElevation(20);
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+        //  popupWindow.setOutsideTouchable(true);
+            /*// dismiss the popup window when touched
+            popupView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    popupWindow.dismiss();
+                    return true;
+                }
+            });*/
+
     }
 }
