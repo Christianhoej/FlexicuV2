97b6aa901e3ea2461e9f23424c1e21b3ebbe1e10
Gunn Hentze <s145494@student.dtu.dk>
2019-01-24 16:49:57 +0100

    diverse

--- app/src/main/java/com/example/chris/flexicuv2/login/LoginScreen_akt.java
+++ app/src/main/java/com/example/chris/flexicuv2/login/LoginScreen_akt.java
@@ -57,17 +57,11 @@ public class LoginScreen_akt extends AppCompatActivity implements View.OnClickLi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-     //   boolean EMULATOR = Build.PRODUCT.contains("sdk") || Build.MODEL.contains("Emulator");
-     //   if (!EMULATOR) {
-     //       Fabric.with(this, new Crashlytics());
-     //   }
-/*        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-        Bundle bundle = new Bundle();
-        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "1");
-        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "Gunn");
-        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
-        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
-*/
+        boolean EMULATOR = Build.PRODUCT.contains("sdk") || Build.MODEL.contains("Emulator");
+        if (!EMULATOR) {
+            Fabric.with(this, new Crashlytics());
+        }
+
         presenter = new LoginPresenter(this, this, loading_screen); //Evt. lav metode der kan sætte presenter på.
         dbManager = new DBManager();
         //logo = findViewById(R.id.flexLogo);
